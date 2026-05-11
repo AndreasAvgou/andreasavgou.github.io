05ebdbcad9105eeb65e23e80c4b947392b8e7643
Andreas Avgoustis <andreasav@alfaedu.gr>
2026-05-11 16:13:27 +0300

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -71,3 +71,9 @@ window.onscroll = function() {
 document.getElementById("scrollTop").onclick = function() {
     window.scrollTo({top: 0, behavior: 'smooth'});
 };
+window.onscroll = function() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("progress-bar").style.width = scrolled + "%";
+};
